6687f8428ece6f9b7e0439544de2776f6ad2478c
pluketic <luketic.petar@gmail.com>
2015-03-22 15:57:05 +0200

    3rd commit3rd commit3rd commit

--- Readme.md
+++ Readme.md
@@ -1,21 +1,29 @@
 # Getting and Cleaning Data: Course Project
+
 ## Introduction
 
 This repository contains exercise for the course project for "Getting and Cleaning data" course, part of the Data Science specialization.
 
 ## About the raw data
+
 Testing and training datasets containing the features are not labeled and can be found in the x_test.txt and x_train.txt. The activity labels are in the y_test.txt and y_train.txt files. Training and testing subjects are stored in the subject_test.txt and subject_train.txt files.
 
 ## About the script and the tidy data set
 
 Input:
+
 1. The UCI HAR Dataset must be downloaded and extracted into “Data” folder within working directory
 
 Execution path:
+
 1. Merges the training and the test sets to create one data set.
+
 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+
 3. Uses descriptive activity names to name the activities in the data set
+
 4. Appropriately labels the data set with descriptive variable names. 
+
 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 Output:
--- run_analysis.R
+++ run_analysis.R
@@ -1,56 +1,60 @@
-setwd("~/AST/Specialization in Data Science/03_Getting And Cleaning The Data/Course Project")
-
+# Read train data
 train = read.csv("Data/train/X_train.txt", sep="", header=FALSE)
 train[,562] = read.csv("Data/train/Y_train.txt", sep="", header=FALSE)
 train[,563] = read.csv("Data/train/subject_train.txt", sep="", header=FALSE)
 
+# Read test data
 test = read.csv("Data/test/X_test.txt", sep="", header=FALSE)
 test[,562] = read.csv("Data/test/Y_test.txt", sep="", header=FALSE)
 test[,563] = read.csv("Data/test/subject_test.txt", sep="", header=FALSE)
 
+# Read Activity labels
 activityLabels = read.csv("Data/activity_labels.txt", sep="", header=FALSE)
 
-# Read features and make the feature names better suited for R with some substitutions
+# Read features and make feature names more desciptive
 features = read.csv("Data/features.txt", sep="", header=FALSE)
+
+# Get only the data on mean and std. dev.
+ColsNeeded <- grep("mean\\(\\)|std\\(\\)", features[, 2])
+
+# Make feature names more desciptive
 features[,2] = gsub('-mean', 'Mean', features[,2])
 features[,2] = gsub('-std', 'Std', features[,2])
 features[,2] = gsub('[-()]', '', features[,2])
-features[,2] = gsub('[,]', '', features[,2])
 
-# Merge train and test datasets together
-allData = rbind(train, test)
 
-# Get only the data on mean and std. dev.
-ColsNeeded <- grep(".*Mean.*|.*Std.*", features[,2])
-
-# First reduce the features table to what we want
+# Shorten features table to the list of variables required
 features <- features[ColsNeeded,]
 
-# Now add the last two columns (subject and activity)
+# Merge train and test datasets into allData dataset
+allData = rbind(train, test)
+
+# Add the last two columns (subject and activity)
 ColsNeeded <- c(ColsNeeded, 562, 563)
 
-# And remove the unwanted columns from allData
+# Remove not required variables from allData
 allData <- allData[,ColsNeeded]
 
 # Add the column names (features) to allData
 colnames(allData) <- c(features$V2, "Activity", "Subject")
-#colnames(allData) <- tolower(colnames(allData))
 
-# Replace Activity IDs with activity names
+# Replace Activity IDs with Activity names
 currentActivity = 1
 for (currentActivityLabel in activityLabels$V2) {
     allData$Activity <- gsub(currentActivity, currentActivityLabel, allData$Activity)
     currentActivity <- currentActivity + 1
 }
 
-
+# Define Activity and Subject variables as factors
 allData$Activity <- as.factor(allData$Activity)
 allData$Subject <- as.factor(allData$Subject)
 
 # Consolidate data into tidy dataset by calculating mean by activity & subject
-tidy = aggregate(allData, by=list(activity = allData$activity, subject=allData$subject), mean)
+tidy = aggregate(allData, by=list(activity = allData$Activity, subject=allData$Subject), mean)
 
-# Remove the subject and activity column, since a mean of those has no use
-tidy[,90] = NULL
-tidy[,89] = NULL
+# Remove not needed variables
+tidy[,70] = NULL
+tidy[,69] = NULL
+
+# Export tidy data set to tab delimited text file
 write.table(tidy, "tidy.txt", sep="\t")
